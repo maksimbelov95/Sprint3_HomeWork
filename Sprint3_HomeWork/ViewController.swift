6ee74ddd48b32b7cbb0175e7f3193453f3adb13b
Maksim Belov <maksimbelov95@mail.ru>
2023-02-22 21:37:17 +0300

    Внесение счетчика? увеличивающего свое значение по нажатию кнопки на 1

--- Sprint3_HomeWork/ViewController.swift
+++ Sprint3_HomeWork/ViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var score: Int = 0
 
+    @IBOutlet weak var buttonScore: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBAction func buttonDidTap(_ sender: Any) {
+        score += 1
+        scoreLabel.text = "Значение счётчика: \(score)"
+        print("Вывод значения в поле Label равное \(score)")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        buttonScore.layer.cornerRadius = 5
     }
+    
 
 
 }
